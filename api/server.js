60af61c5f5a6b401474ec6e0191ae369ae9ba4dc
arsalan rahimi <years1373@yahoo.com>
2022-01-21 13:22:13 -0800

    server and tests running

--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
-// build your server here and require it from index.js
+const express = require("express")
+
+const server = express()
+
+server.use(express.json())
+
+server.get("/", (req, res) => {
+  res.status(200).json("Hello world")
+})
+//eslint-disable-next-line
+server.use("*", (err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    status: err.status,
+    error: "something went wrong",
+  })
+})
+
+module.exports = server
