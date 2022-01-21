a1f3b0615fd4522f459ae1d69c5ddcff63ff5cdb
arsalan rahimi <years1373@yahoo.com>
2022-01-21 14:27:56 -0800

    projects completed

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,25 @@
-// build your `/api/projects` router here
+const router = require("express").Router()
+const rp = require("./model")
+const { projectName } = require("./projects-middleware")
+const { makeBolean } = require("../../helper/makeBoolean")
+
+router.get("/", async (req, res, next) => {
+  try {
+    const allprojects = await rp.getAll()
+    const projects = allprojects.map((project) => {
+      makeBolean(project)
+      return project
+    })
+    res.status(200).json(projects)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post("/", projectName, async (req, res) => {
+  const newProject = await rp.insert(req.body)
+  makeBolean(newProject)
+  res.status(201).json(newProject)
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const express = require("express")
+const projectRouter = require("./project/router")
 
 const server = express()
 
 server.use(express.json())
+
+server.use("/api/projects", projectRouter)
 
 server.get("/", (req, res) => {
   res.status(200).json("Hello world")
